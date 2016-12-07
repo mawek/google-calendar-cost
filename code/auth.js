b5374033f0ed6b4eccac816967a938ffb3af9a41
jdenk <jdenk@seznam.cz>
2016-12-07 14:29:59 +0100

    Request

--- code/auth.js
+++ code/auth.js
@@ -1,5 +1,25 @@
-console.log(chrome.identity);
 
-chrome.identity.getAuthToken({  }, function(token) {
-    console.log("token: " + token);
+chrome.identity.getAuthToken({'interactive': true }, function(token) {
+    makeRequest(token, "GET", "https://www.googleapis.com/calendar/v3/calendars/primary/events/?key=AIzaSyBcfRwpSJj1f7xtt7KZqeZyQ5Qt_LWOsoA",
+        function (response) {
+            alert(response)
+        });
 });
+
+
+makeRequest = function(token, method, url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+
+    xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+
+    xhr.onload = function() {
+        callback(xhr.response);
+    }
+
+    xhr.onerror = function(e) {
+        console.log(this, this.status, this.response,
+            this.getAllResponseHeaders());
+    };
+    xhr.send({});
+};
